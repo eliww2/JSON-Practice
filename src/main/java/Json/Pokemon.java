134818aa914417999f81241f3338ece5aa9ae303
eliww2 <eliww2@illinois.edu>
2021-02-05 15:35:55 -0600

    Added the different fields applicable to pokemon.

--- src/main/java/Json/Pokemon.java
+++ src/main/java/Json/Pokemon.java
@@ -1,4 +1,20 @@
 package Json;
 
 public class Pokemon {
+    private int id;
+    private String num; //change name
+    private String name;
+    private String img; // change name
+    private String[] type;
+    private String height;
+    private String weight;
+    private String candy;
+    private int candy_count;     // I want to change this name to candyToEvolve
+    private String egg; // change to eggDistance
+    private double spawn_chance;
+    private String spawn_time;
+    private double[] multipliers;
+    private String[] weaknesses;
+
+
 }
